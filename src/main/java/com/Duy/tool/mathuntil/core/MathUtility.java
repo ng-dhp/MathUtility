8ed6801d9c6e8c7ec1d0d5fcf516acacb7598efd
ng-dhp <duynguyen17122004@gmail.com>
2025-06-12 17:08:34 +0700

    update main code to use recursion and to see green 4even

--- src/main/java/com/Duy/tool/mathuntil/core/MathUtility.java
+++ src/main/java/com/Duy/tool/mathuntil/core/MathUtility.java
@@ -8,18 +8,43 @@ public class MathUtility {
     // KHÔNG CÓ GIAI THỪA SỐ ÂM
     // 0! =1;
     // 20! VỪA ĐỦ KIỂU LONG, TỨC LÀ 21! VƯỢT KIỂU LONG
-    public static long getFactorial(int n){
+
+        public static long getFactorial(int n){
         // đem code chinh ra sửa gọi là refactor
         if (n<0 || n>20){
             throw new IllegalArgumentException("n must be between 0 and 20");
         } // bạn đưa invalid data, tớ ném ra ngoại lệ, hàm không được chạy thành công ta value trở về
-        if (n==0){
+        if (n==0 || n==1){
             return 1;
         }
-        long result = 1; // thuật toán dồn con heo đất
-        for (int i = 1; i <= n;i++){
-            result *= i; // nhân dồn vào từng cháu i
-        }
-        return result;
+//        long result = 1; // thuật toán dồn con heo đất
+//        for (int i = 1; i <= n;i++){
+//            result *= i; // nhân dồn vào từng cháu i
+//        }
+        return n * getFactorial(n-1);
     }
+
+    // ĐEM LÊN SERVER MÀ RA MÀU XANH
+        //1. CODE TỐI ƯU NGON VÌ PASS TEST CASE
+        //2. TEST LẠI THỨ TỪNG TEST RẤT MẤT CÔNG, NAY CÓ SCRIPT TEST TỰ ĐỘNG, KHỎE RE
+        // RECURSION TESTING, KIỂM THỬ HỒI QUY
+
+    // đệ quy: recursion gọi lại chính mình với quy mô nhỏ hơn
+
+
+
+//    public static long getFactorial(int n){
+//        // đem code chinh ra sửa gọi là refactor
+//        if (n<0 || n>20){
+//            throw new IllegalArgumentException("n must be between 0 and 20");
+//        } // bạn đưa invalid data, tớ ném ra ngoại lệ, hàm không được chạy thành công ta value trở về
+//        if (n==0){
+//            return 1;
+//        }
+//        long result = 1; // thuật toán dồn con heo đất
+//        for (int i = 1; i <= n;i++){
+//            result *= i; // nhân dồn vào từng cháu i
+//        }
+//        return result;
+//    }
 }
